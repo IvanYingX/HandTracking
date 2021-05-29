f8105457562fff88df019ea62a16253ca0c169c6
IvanYingX <ying.yingxuan@hotmail.com>
2021-05-29 02:16:43 +0200

    Cleaned the code and added a landmark identifier

--- HandTracking.py
+++ HandTracking.py
@@ -4,12 +4,11 @@ import time
 
 class HandTracking:
     def __init__(self, mode=False, hands=2, detect_conf=0.5, tracking_conf=0.5):
-        mpHands = mp.solutions.hands
-        self.hands = mpHands.Hands(mode, hands, detect_conf, tracking_conf)
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(mode, hands, detect_conf, tracking_conf)
         self.mpDraw = mp.solutions.drawing_utils
 
     def detect_hand(self, img, draw=True):
-        time_0 = 0
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = self.hands.process(imgRGB)
 
@@ -19,3 +18,18 @@ class HandTracking:
                     self.mpDraw.draw_landmarks(img,
                                            handLMs,
                                            self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def identify_point(self, img, point=0, hand=0, draw=True):
+        h, w, c = img.shape
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
+        lm_list = []
+        if results.multi_hand_landmarks:
+            my_hand = results.multi_hand_landmarks[hand]
+            lm = my_hand.landmark[point]
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            lm_list = [cx, cy]
+            cv2.circle(img, (lm_list[0], lm_list[1]),
+                   10, (255, 0, 255), cv2.FILLED)
+        return lm_list
--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import cv2
 import mediapipe as mp
 import time
+from HandTracking import HandTracking
+
 cap = cv2.VideoCapture(0)
-
+time_0 = 0
+hand = HandTracking()
+i = 0
 while True:
-    success, img = cap.read()
-    h, w, c = img.shape
-    
-    
-    
-            # wrist = handLMs.landmark[0]
-            # cx, cy = int(wrist.x * w), int(wrist.y * h)
-            # cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-
     time_c = time.time()
     f = 1 / (time_c - time_0)
     time_0 = time_c
-    cv2.putText(img, str(int(f)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    img = hand.detect_hand(img)
+    lm_list = hand.identify_point(img, point=8)
+    if lm_list:
+        print(lm_list)
+
+    else:
+        print('Not found')
+    cv2.putText(img, 'fps = ' + str(int(f)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
     cv2.imshow('Image', img)
-    cv2.waitKey(1)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
